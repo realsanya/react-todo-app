e4800846eee216f7e4377afad417a9d7ab293e8e
Morozova Aleksandra <sashamorozova01@mail.ru>
2021-03-24 23:57:55 +0300

    init

---
+++ todo-app/src/components/list/index.js
@@ -0,0 +1,27 @@
+import TodoListItem from '../item';
+import './styles.css';
+
+const TodoList = ({ 
+    items, 
+    onToggleImportant, 
+    onToggleDone, 
+    onDelete 
+}) => {
+
+  const elements = items.map((item) => {
+    const { id, ...itemProps } = item;
+    return (
+      <li key={id} className="list-group-item">
+        <TodoListItem
+          { ...itemProps }
+          onToggleImportant={ () => onToggleImportant(id) }
+          onToggleDone={ () => onToggleDone(id) }
+          onDelete={ () => onDelete(id) } />
+      </li>
+    );
+  });
+
+  return (<ul className="todo-list list-group">{ elements }</ul>);
+};
+
+export default TodoList;
